8c9d97ca765b8e28bd966d2b0bd50e0fbec0e844
mikeadly <73313912+mikeadly@users.noreply.github.com>
2023-03-02 10:34:35 +0200

    Add documentation to linked_list.h

--- c_linked_list/linked_list.h
+++ c_linked_list/linked_list.h
@@ -1,11 +1,20 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
+
+
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
+/**
+ * Fuction Options
+ */
 #define APPEND "ap" 
 #define PREPEND "pre" 
 #define DELETE_FIRST "df" 
@@ -19,6 +28,8 @@
 #define REVERSE "rev"
 #define GET_INDEX "get"
 #define QUIT "q"
+
+
 
 struct node
 {
@@ -34,23 +45,126 @@ struct ll
 };
 
 
+/**
+ * Allocates and returns pointer of Node.
+ */
 struct node * new_node(uint8_t);
+
+
+/**
+ * Allocates and returns pointer to linked list.
+ */
 struct ll* init_ll ();
+
+
+/**
+ * Prints Length Value of linked list.
+ */
 void print_length (struct ll*);
+
+
+/**
+ * Prints tail value of linked list.
+ */
 void print_tail (struct ll*);
+
+
+/**
+ * Prints head value of linked list.
+ */
 void print_head (struct ll*);
+
+
+/**
+ * Creates then adds new node before head.
+ */
 void append (struct ll*, uint8_t);
-void delete_first(struct ll*);
-void del_ll (struct ll*);
+
+
+/**
+ * Creates then adds new node after tail.
+ */
 void prepend (struct ll*, uint8_t);
+
+
+/**
+ * Delete the first node.
+ */
+void delete_first(struct ll*);
+
+
+/**
+ * Delete last node.
+ */
 void delete_last (struct ll*);
+
+
+/**
+ * Delete all the nodes of the linked list.
+ */
+void del_ll (struct ll*);
+
+
+/**
+ * Prints all the nodes of the linked list.
+ */
 void print_list(struct ll*);
+
+
+/**
+ * Returns a value of a given index.
+ */
 int8_t get_index(struct ll*, int8_t);
-uint8_t enter_value();
-int8_t enter_index();
-void lst_append(struct ll*);
-void lst_prepend(struct ll*);
+
+
+/**
+ * Reverse all nodes
+ */
 void reverse(struct ll*);
+
+
+/*******************************************/
+
+
+/**************************
+ *    HELPER FUNCTIONS    *
+ **************************/
+
+
+/**
+ * Interactive fuction/ prints message
+ * then takes value form end user.
+ */
+uint8_t enter_value();
+
+
+/**
+ * Interactive fuction/prints massage
+ * then take index from the user.
+ */
+int8_t enter_index();
+
+
+/**
+ * Interactive function/prints message
+ * then take value then append it.
+ */
+void lst_append(struct ll*);
+
+
+/**
+ * Interactive fuction/ prints message
+ * the take value the prepend it.
+ */
+void lst_prepend(struct ll*);
+
+
+/**
+ * Interactive fuction/ prints confirmation
+ * message and return options.
+ */
 uint8_t confirm_deletion();
+
+
 
 #endif //LINKED_LIST_H
